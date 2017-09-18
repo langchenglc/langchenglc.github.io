61c3cfc03d190246eac8a3785a36ff36ecac453f
langchenglc <langchenglc@qq.com>
2017-09-18 11:35:54 +0800

    modify showmain.js

--- js/show-main.js
+++ js/show-main.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(document).ready(function(){
-		var show-main="<div style='position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"+
+		var show_main="<div style='position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"+
 			+"<a title='返回博客主页' href='../../'>返回博客主页</a></div>";
 		$(document).append(show-main);
 	})
