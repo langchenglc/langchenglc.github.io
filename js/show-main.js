e37a6a93597cae90824c5f51390ecd290987e073
浪尘 <langchenglc@qq.com>
2017-09-18 12:18:00 +0800

    modify-show-mian.js
    modify-show-mian.js

--- js/show-main.js
+++ js/show-main.js
@@ -2,6 +2,6 @@ $(function(){
 	$(document).ready(function(){
 		var show_main="<div style='position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"+
 			+"<a title='返回博客主页' href='../../'>返回博客主页</a></div>";
-		$(document).append(show_main);
+		$(document.body).append(show_main);
 	})
 });
