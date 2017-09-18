4617736701774c3f5d933c738bcaa4a788e52edb
langchenglc <langchenglc@qq.com>
2017-09-18 12:51:44 +0800

    modify

--- js/show-main.js
+++ js/show-main.js
@@ -1,9 +1,9 @@
 $(function(){
 	$(document).ready(function(){
-		var to_main="<div width='50px;' height='50px;' style='position: fixed; bottom: 80px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
-			+"<a title='返回顶部' href='#'><img src='../images/to-top.png'/></a></div>";
-		var to_top="<div width='50px;' height='50px;' style='position: fixed; bottom: 50px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
-			+"<a title='博客主页' href='../../'><img src='../images/to-home.png'/></a></div>";
+		var to_main="<div  style='position: fixed; bottom: 80px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
+			+"<a title='返回顶部' href='#'><img width='50px;' height='50px;' src='../images/to-top.png'/></a></div>";
+		var to_top="<div style='position: fixed; bottom: 50px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
+			+"<a title='博客主页' href='../../'><img width='50px;' height='50px;' src='../images/to-home.png'/></a></div>";
 		$(document.body).append(to_main);
 		$(document.body).append(to_top);
 	})
