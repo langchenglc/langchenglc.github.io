c56a22917e7efb40574aba7b1cdf5a1d5b14a63e
langchenglc <langchenglc@qq.com>
2017-09-18 11:34:06 +0800

    to mian

--- js/show-main.js
+++ js/show-main.js
@@ -1,6 +1,7 @@
 $(function(){
 	$(document).ready(function(){
-		var show-main="<div style="position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;">返回博客主页</div>";
+		var show-main="<div style='position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"+
+			+"<a title='返回博客主页' href='../../'>返回博客主页</a></div>";
 		$(document).append(show-main);
 	})
 });
