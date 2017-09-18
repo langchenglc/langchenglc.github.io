e4fc4f3b23489d91c2f49801a8bacba451c0316d
langchenglc <langchenglc@qq.com>
2017-09-18 11:25:11 +0800

    to main

---
+++ js/show-main.js
@@ -0,0 +1,6 @@
+$(function(){
+	$(document).ready(function(){
+		var show-main="<div style="position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;">返回博客主页</div>";
+		$(document).append(show-main);
+	})
+});
