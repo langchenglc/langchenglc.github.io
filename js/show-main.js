bbeb8c62705a06942c46c6706fc2d5f3c0c0d805
浪尘 <langchenglc@qq.com>
2017-09-18 12:23:08 +0800

    modify show mian.js

--- js/show-main.js
+++ js/show-main.js
@@ -1,7 +1,7 @@
 $(function(){
 	$(document).ready(function(){
-		var show_main="<div style='position: fixed; bottom: 50px; right: 15px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"+
-			+"<a title='返回博客主页' href='../../'>返回博客主页</a></div>";
+		var show_main="<div style='position: fixed; bottom: 50px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
+			+"<a title='博客主页' href='../../'>返回主页</a></div>";
 		$(document.body).append(show_main);
 	})
 });
