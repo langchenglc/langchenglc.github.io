a28f4c521252253f90359a8be5deb34fcf5fd7e7
langchenglc <langchenglc@qq.com>
2017-09-19 11:12:10 +0800

    modify links

--- js/show-main.js
+++ js/show-main.js
@@ -5,7 +5,7 @@ $(function(){
 		var to_top="<div style='position: fixed; bottom: 50px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
 			+"<a title='博客主页' href='../../'><img width='40px;' height='40px;' src='../images/to-home.png'/></a></div>";
 		var to_leanote="<div style='position: fixed; bottom: 100px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
-			+"<a title='leanote blog' href='http://gong.leanote.com'><img width='40px;' height='40px;' src='../images/to-link.png'/></a></div>";
+			+"<a title='leanote blog' href='http://leanote.com/blog/57f84ebcab644107bb00b9b6'><img width='40px;' height='40px;' src='../images/to-link.png'/></a></div>";
 		var to_wechat="<div style='position: fixed; bottom: 150px; right: 30px; cursor: pointer; z-index: 999999; display: block; opacity: 1;'>"
 			+"<a title='博主微信' target='_blank' href='../images/wechat.jpg'><img width='40px;' height='40px;' src='../images/to-wechat.png'/></a></div>";
 		$(document.body).append(to_main);
